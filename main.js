b0536f7e481f1fc215d4521654a4d7f05216960f
Vytautas Rakauskas <vyt.rakauskas@gmail.com>
2024-12-12 22:16:56 +0200

    Pagaliau veikia viskas viename, beliko i6skaidyt

--- main.js
+++ main.js
@@ -1,7 +1,72 @@
-import User from "./user.js";
-import { printName, printAge } from "./user.js";
+// Inicializuoti kintamuosius DOM elementams
+const ekranas = document.getElementById('display');
+const mygtukai = document.querySelectorAll('.btn');
+let pirmasSkaicius = '';
+let veiksmas = '';
+let antrasSkaicius = '';
 
-const user = new User("Bob", 35);
 
-printName(user);
-printAge(user);
+mygtukai.forEach(mygtukas => {
+    mygtukas.addEventListener('click', () => {
+        const reiksme = mygtukas.textContent;
+        if (mygtukas.classList.contains('number')) {
+            apdorotiSkaiciu(reiksme);
+        } else if (mygtukas.classList.contains('operator')) {
+            apdorotiVeiksma(reiksme);
+        } else if (mygtukas.id === 'equals') {
+            atliktiSkaiciavima();
+        } else if (mygtukas.id === 'clear') {
+            isvalytiEkrana();
+        }
+        
+    });
+});
+
+function apdorotiSkaiciu(skaicius) {
+    if (veiksmas === '') {
+    pirmasSkaicius += skaicius;
+    atnaujintiEkrana(pirmasSkaicius);
+    } else {
+        antrasSkaicius += skaicius;
+        atnaujintiEkrana(antrasSkaicius);
+    }
+}
+
+function apdorotiVeiksma(veiksmai) {
+    if (pirmasSkaicius === '') return;
+    veiksmas = veiksmai;
+    }
+
+function atliktiSkaiciavima() {
+    let rezultatas;
+    const pirmas = parseFloat(pirmasSkaicius);
+    const antras = parseFloat(antrasSkaicius);
+
+    if (isNaN(pirmas) || isNaN(antras)) return;
+
+    switch (veiksmas) {
+        case '+':
+            rezultatas = pirmas + antras;
+            break;
+        case '-':
+            rezultatas = pirmas - antras;
+            break;
+        default:
+            return;        
+    }
+    atnaujintiEkrana(rezultatas);
+    pirmasSkaicius = rezultatas.toString();
+    antrasSkaicius = '';
+    veiksmas = '';
+}
+
+function atnaujintiEkrana(rezultatas) {
+    ekranas.textContent = rezultatas;
+}
+
+function isvalytiEkrana() {
+    pirmasSkaicius = '';
+    veiksmas = '';
+    antrasSkaicius = '';
+    atnaujintiEkrana('0');
+}
