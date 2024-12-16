77ed3de8cd3709dc6b49e75c5ead3c9517c4e87b
Vytautas Rakauskas <vyt.rakauskas@gmail.com>
2024-12-16 19:21:27 +0200

    iskelta skaiciuotuvo funkcijos

--- calculator.js
+++ calculator.js
@@ -1,14 +1,55 @@
-export default class User {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
+import { atliktiSudetis, atliktiAtimtis } from "./mathOperations.js";
+import { ekranas } from "./domElements.js";
+
+let pirmasSkaicius = '';
+let veiksmas = '';
+let antrasSkaicius = '';
+
+export function apdorotiSkaiciu(skaicius) {
+    if (veiksmas === '') {
+    pirmasSkaicius += skaicius;
+    atnaujintiEkrana(pirmasSkaicius);
+    } else {
+        antrasSkaicius += skaicius;
+        atnaujintiEkrana(antrasSkaicius);
+    }
 }
 
-export const printName = (user) => {
-  console.log(`User's name is ${user.name}`);
-};
+export function apdorotiVeiksma(veiksmai) {
+    if (pirmasSkaicius === '') return;
+    veiksmas = veiksmai;
+}
+    
+export function atliktiSkaiciavima() {
+    let rezultatas;
+    const pirmas = parseFloat(pirmasSkaicius);
+    const antras = parseFloat(antrasSkaicius);
 
-export const printAge = (user) => {
-  console.log(`User's is ${user.age} years old`);
-};
+    if (isNaN(pirmas) || isNaN(antras)) return;
+
+    switch (veiksmas) {
+        case '+':
+            rezultatas = atliktiSudetis(pirmas, antras);
+            break;
+        case '-':
+            rezultatas = atliktiAtimtis(pirmas, antras);
+            break;
+        default:
+            return;        
+    }
+    atnaujintiEkrana(rezultatas);
+    pirmasSkaicius = rezultatas.toString();
+    antrasSkaicius = '';
+    veiksmas = '';
+}
+
+export function atnaujintiEkrana(rezultatas) {
+    ekranas.textContent = rezultatas;
+}
+
+export function isvalytiEkrana() {
+    pirmasSkaicius = '';
+    veiksmas = '';
+    antrasSkaicius = '';
+    atnaujintiEkrana('0');
+}
--- main.js
+++ main.js
@@ -1,58 +1,10 @@
-import { atliktiSudetis, atliktiAtimtis } from './mathOperations.js';
-import { ekranas, mygtukai, mygtukuKlausytojai } from './domElements.js';
 
-let pirmasSkaicius = '';
-let veiksmas = '';
-let antrasSkaicius = '';
+import { mygtukai, mygtukuKlausytojai } from './domElements.js';
+import {
+    apdorotiSkaiciu, apdorotiVeiksma, atliktiSkaiciavima,
+    isvalytiEkrana
+} from './calculator.js';
+
 
 mygtukuKlausytojai(mygtukai, apdorotiSkaiciu, apdorotiVeiksma,
     atliktiSkaiciavima, isvalytiEkrana);
-
-function apdorotiSkaiciu(skaicius) {
-    if (veiksmas === '') {
-    pirmasSkaicius += skaicius;
-    atnaujintiEkrana(pirmasSkaicius);
-    } else {
-        antrasSkaicius += skaicius;
-        atnaujintiEkrana(antrasSkaicius);
-    }
-}
-
-function apdorotiVeiksma(veiksmai) {
-    if (pirmasSkaicius === '') return;
-    veiksmas = veiksmai;
-    }
-
-function atliktiSkaiciavima() {
-    let rezultatas;
-    const pirmas = parseFloat(pirmasSkaicius);
-    const antras = parseFloat(antrasSkaicius);
-
-    if (isNaN(pirmas) || isNaN(antras)) return;
-
-    switch (veiksmas) {
-        case '+':
-            rezultatas = atliktiSudetis(pirmas, antras);
-            break;
-        case '-':
-            rezultatas = atliktiAtimtis(pirmas, antras);
-            break;
-        default:
-            return;        
-    }
-    atnaujintiEkrana(rezultatas);
-    pirmasSkaicius = rezultatas.toString();
-    antrasSkaicius = '';
-    veiksmas = '';
-}
-
-function atnaujintiEkrana(rezultatas) {
-    ekranas.textContent = rezultatas;
-}
-
-function isvalytiEkrana() {
-    pirmasSkaicius = '';
-    veiksmas = '';
-    antrasSkaicius = '';
-    atnaujintiEkrana('0');
-}
